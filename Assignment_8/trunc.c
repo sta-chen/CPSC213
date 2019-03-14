a06676638ebad3abdd84a90a4eb69bd06e933b36
Sta_C <sta_chen@github.com>
2019-03-14 15:47:45 -0700

    q2.s finished

---
+++ Assignment_8/trunc.c
@@ -0,0 +1,14 @@
+//
+//  trunc.c
+//  
+//
+//  Created by Sta_C on 2019-03-13.
+//
+
+#include <stdio.h>
+#include "list.h"
+
+// Step 2
+void stringToInteger(element_t* out, element_t in) {
+    
+}
